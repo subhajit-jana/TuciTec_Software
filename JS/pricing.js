64c5077a80d390c8338f9f9eed113f30c95af1f6
subhajit <subhajitjana330@gmail.com>
2025-01-14 18:39:52 +0530

    pricing page

---
+++ JS/pricing.js
@@ -0,0 +1,102 @@
+// effect 
+const single_proudn343443=document.getElementById('single_proudn343443');
+const bundle_product343=document.getElementById('bundle_product343');
+// clickcd
+const single_proudn343443_click=document.getElementById('single_proudn343443_click');
+const bundle_product343_click=document.getElementById('bundle_product343_click');
+
+
+
+single_proudn343443_click.addEventListener('click',()=>{
+    single_proudn343443.classList.remove("d-none");
+    bundle_product343.classList.add('d-none');
+    // button 
+    // add 
+    single_proudn343443_click.classList.add("bg_dffd4563white");
+    bundle_product343_click.classList.add("bg_dfere344");
+    // remove 
+    single_proudn343443_click.classList.remove("bg_dfere344");
+    bundle_product343_click.classList.remove("bg_dffd4563white");
+    console.log('sing')
+   });
+bundle_product343_click.addEventListener('click',()=>{
+    single_proudn343443.classList.add("d-none");
+    bundle_product343.classList.remove("d-none");
+    // button 
+   // button 
+   // add
+   single_proudn343443_click.classList.add("bg_dfere344");
+   bundle_product343_click.classList.add("bg_dffd4563white");
+   // remove 
+   single_proudn343443_click.classList.remove("bg_dffd4563white");
+   bundle_product343_click.classList.remove("bg_dfere344");
+   
+   console.log('bundle')
+
+   
+   });
+
+//boundle_show
+// click area 
+const monthlySelect=document.getElementById('monthlySelect');
+const yearlySelect=document.getElementById('yearlySelect');
+const lifeTimeselect=document.getElementById('lifeTimeselect');
+//show area
+const monthlyshow=document.getElementById('monthlyshow');
+const yearlyshow=document.getElementById('yearlyshow');
+const lifetimeshow=document.getElementById('lifetimeshow');
+
+
+
+
+
+
+
+monthlySelect.addEventListener("click",()=>{
+    monthlyshow.classList.remove('d-none');
+    yearlyshow.classList.add('d-none');
+    lifetimeshow.classList.add('d-none');
+    // 
+    monthlySelect.classList.add('bg_2332danger');
+    yearlySelect.classList.remove('bg_2332danger');
+    lifeTimeselect.classList.remove('bg_2332danger');
+
+
+})
+
+
+yearlySelect.addEventListener("click",()=>{
+    monthlyshow.classList.add('d-none');
+    yearlyshow.classList.remove('d-none');
+    lifetimeshow.classList.add('d-none');
+    //
+    monthlySelect.classList.remove('bg_2332danger');
+    yearlySelect.classList.add('bg_2332danger');
+    lifeTimeselect.classList.remove('bg_2332danger');
+
+})
+
+lifeTimeselect.addEventListener("click",()=>{
+    monthlyshow.classList.add('d-none');
+    yearlyshow.classList.add('d-none');
+    lifetimeshow.classList.remove('d-none');
+    //
+    monthlySelect.classList.remove('bg_2332danger');
+    yearlySelect.classList.remove('bg_2332danger');
+    lifeTimeselect.classList.add('bg_2332danger');
+
+
+})
+// faq 
+const clickfaq_area=document.querySelectorAll(".faqw34");
+const showhide=document.querySelectorAll(".showhide5454");
+function faqcontetn(click,forcals){
+    click.addEventListener("click",()=>{
+        forcals.classList.toggle('d-none')
+        click.classList.toggle('addhovercollor')
+    })   
+}
+for(let i=0; i<=clickfaq_area.length;i++){
+    faqcontetn(clickfaq_area[i],showhide[i]);
+}
+//
